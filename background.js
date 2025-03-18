dfaa1905fb2fae43f7a7ce4a1fa67edfd4da18a5
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2025-03-18 23:14:49 +0900

    Refactor

--- background.js
+++ background.js
@@ -8,16 +8,12 @@ chrome.runtime.onMessage.addListener(
 				break;
 			case 'get_video_data':
 				const key = 'AIzaSyAgQilpJEVbK4gC8uaoNuSSFg_VqBVg-0Q';
-				var vide_id = encodeURIComponent(request.video_id);
-				var url = `https://www.googleapis.com/youtube/v3/videos?id=${vide_id}&key=${key}&part=snippet,liveStreamingDetails`;
+				let vide_id = encodeURIComponent(request.video_id);
+				let url = `https://www.googleapis.com/youtube/v3/videos?id=${vide_id}&key=${key}&part=snippet,liveStreamingDetails`;
 				fetch(url)
 					.then(response => response.json())
-					.then(data => {
-						sendResponse({data: data});
-					})
-					.catch((error) => {
-						console.log(error);
-					});
+					.then(data => sendResponse({data: data}))
+					.catch((error) => console.log(error));
 				break;
 			default:
 				console.log('unknown request');
